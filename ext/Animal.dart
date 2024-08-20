fa270459d0896c7fadd32a313a7274161fa35718
Jas Addison <64211121+jaddison06@users.noreply.github.com>
2024-08-20 15:36:25 +0100

    Animal food !!!!!

--- ext/Animal.dart
+++ ext/Animal.dart
@@ -1,5 +1,17 @@
 import 'generated.dart';
 
+final SAFE_FOOD = {
+  AnimalType.Cat: [FoodType.Fish],
+  AnimalType.Dog: [FoodType.Bone, FoodType.Fish],
+  AnimalType.Horse: [FoodType.Grape, FoodType.Ketamine],
+  AnimalType.Octopus: [FoodType.Fish, FoodType.Grape]
+};
+
 class Animal extends AnimalBase {
+  Animal({required String uuid, required AnimalType type, required String name})
+    : super(uuid: uuid, type: type, name: name);
   
+  void feed(FoodType food) {
+    if (!SAFE_FOOD[type]!.contains(food)) throw APIException.AnimalDied;
+  }
 }
